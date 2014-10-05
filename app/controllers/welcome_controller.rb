6429796f0d27e2427ae40eef4029adec7bc6d9f7
Nathan Pointer <nwpointer@gmail.com>
2014-10-05 13:12:26 -0700

    animated flash msg

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,12 +5,14 @@ class WelcomeController < ApplicationController
   def post
   	# get input
   	@quantity = params[:email]
-  	flash[:notice] = "#{@quantity}"
+  	
+  	# save to db
   	@user = EmailList.create(email:"#{@quantity}")
  
-  
-  	# save to db
+  	# validation
+  	
   	# flash a sucess msg
+  	flash[:notice] = "thanks! we'll let you know as soon as we launch!"
   	render 'index'
   end
 end
